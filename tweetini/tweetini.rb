a464422ab3d77d9c68309466a60210220022f77b
Ellen Marie Dash <me@duckie.co>
2015-08-14 20:48:58 -0400

    Remove 3 characters from tweetini.

--- tweetini/tweetini.rb
+++ tweetini/tweetini.rb
@@ -1,2 +1,2 @@
 # INI parser
-def tini(x)s={};c=:global;x.split("\n").map{|l|l=~/^\[(.*)\]$/? c=$1:(l=~/^\s*(.*)\s*=\s*(.*?)\s*$/&&(s[c]||={})[$1]=$2)};s;end
+def tini(x)s={};c=:global;x.split("\n").map{|l|l=~/^\[(.*)\]$/?c=$1:(l=~/^\s*(.*)\s*=(.*)/&&(s[c]||={})[$1]=$2.strip)};s;end
