a2dfe57bdbba6a8944632ba647f70fe88f663af0
Ellen Marie Dash <me@duckie.co>
2017-09-09 22:12:46 -0400

    slightly longer version that works without warnings

--- linked-list/linked-list.h
+++ linked-list/linked-list.h
@@ -1,1 +1,1 @@
-*malloc();typedef struct{int*p,*d,*n;}l;l*n(*d){l*x=malloc(24);x->d=d;return x;}c(l*p,l*n){if(p)p->n=n;if(n)n->p=p;}
+void*malloc();typedef struct{void*p,*d,*n;}l;l*n(void*d){l*x=malloc(24);x->d=d;return x;}void c(l*p,l*n){if(p)p->n=n;if(n)n->p=p;}
