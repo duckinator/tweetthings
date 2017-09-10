d7515888bc098d25571df634a346dedcb9e6ecae
Ellen Marie Dash <me@duckie.co>
2017-09-09 21:40:31 -0400

    ?!

--- linked-list/test-all.c
+++ linked-list/test-all.c
@@ -1,28 +1,66 @@
 #//\
-rm -f test && clang -std=c99 -I. -DDEBUG test-all.c -o test-all && ./test-all && rm test-all; exit
+    rm -f test && clang -std=c99 -I. -DDEBUG test-all.c -o test-all && ./test-all && rm test-all; exit
 
 #include <all.h>
 #include <stdio.h>
 #include <string.h>
 
 int main() {
-  l *first,
-    *last;
+    l *first,
+      *middle,
+      *last;
 
-  char *str1 = malloc(6),
-       *str2 = malloc(7);
+    char *first_str  = malloc(7),
+         *middle_str = malloc(8),
+         *last_str   = malloc(7);
 
-  last  = n();
-  first = last;
+    l *current;
+    int i;
 
-  strncpy(str1, "last!", 6);
-  last->d = str1;
+    // Create nodes.
+    strncpy(first_str, "first!", 7);
+    first = n(first_str);
 
-  strncpy(str2, "first!", 7);
-  first = p(last, str2);
+    strncpy(middle_str, "middle!", 7);
+    middle = n(middle_str);
 
-  printf("%s\n", (char*)first->d);
-  printf("%s\n", (char*)first->n->d);
+    strncpy(last_str, "last!", 7);
+    last = n(last_str);
 
-  return 0;
+
+    // Connect nodes.
+    c(NULL, first);
+    c(first, middle);
+    c(middle, last);
+    c(last, NULL);
+
+    // Iterate over nodes.
+    printf("Forward iteration from `first`:\n");
+
+    i = 1;
+    current = first;
+    while (1) {
+        printf("  %i: %s\n", i, current->d);
+        i++;
+        current = current->n;
+
+        if (current == NULL)
+            break;
+    }
+
+    printf("\n\n");
+
+    printf("Reverse iteration from `last`:\n");
+    i = 3;
+    current = last;
+    while (1) {
+        printf("  %i: %s\n", i, current->d);
+        i--;
+        current = current->p;
+
+        if (current == NULL)
+            break;
+    }
+
+    return 0;
 }
