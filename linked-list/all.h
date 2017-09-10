20e107f92d2726ce6d666a1abe88ee00931e6195
Ellen Marie Dash <me@duckie.co>
2017-09-09 21:40:31 -0400

    ?!

--- linked-list/all.h
+++ linked-list/all.h
@@ -1,4 +1,3 @@
 #include <stdlib.h>
 #define l struct l
-#define n() malloc(24)
-l{l*p;void*d;l*n;};l*p(l*N,void*d){l*x=n();x->d=d;x->n=N;return N->p=x;}l*a(l*p,void*d){l*x=n();x->d=d;p->n=x;return x->p=p;}
+l{l*p;void*d;l*n;};l*n(void*d){l*x=malloc(24);x->d=d;return x;}void c(l*p,l*n){if(p!=0)p->n=n;if(n!=0)n->p=p;}
