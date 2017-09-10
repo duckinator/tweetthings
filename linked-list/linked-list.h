b67ed0147abc917c5b16778503d128e4513223cf
Ellen Marie Dash <me@duckie.co>
2017-09-09 22:16:15 -0400

    actually use sizeof() instead of hard-coding the size

--- linked-list/linked-list.h
+++ linked-list/linked-list.h
@@ -1,1 +1,1 @@
-void*malloc();typedef struct{void*p,*d,*n;}l;l*n(void*d){l*x=malloc(24);x->d=d;return x;}void c(l*p,l*n){if(p)p->n=n;if(n)n->p=p;}
+void*malloc();typedef struct{void*p,*d,*n;}l;l*n(void*d){l*x=malloc(sizeof(l));x->d=d;return x;}void c(l*p,l*n){if(p)p->n=n;if(n)n->p=p;}
