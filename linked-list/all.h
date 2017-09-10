c6a71afa087b6fd4897db0ba754e62d3976bdba1
Ellen Marie Dash <me@duckie.co>
2017-09-09 21:47:34 -0400

    143 characters!

--- linked-list/all.h
+++ linked-list/all.h
@@ -1,3 +1,2 @@
-#include <stdlib.h>
 #define l struct l
-l{l*p;void*d;l*n;};l*n(void*d){l*x=malloc(24);x->d=d;return x;}void c(l*p,l*n){if(p!=0)p->n=n;if(n!=0)n->p=p;}
+void*malloc();l{l*p;void*d;l*n;};l*n(void*d){l*x=malloc(24);x->d=d;return x;}void c(l*p,l*n){if(p!=0)p->n=n;if(n!=0)n->p=p;}
